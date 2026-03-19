c6aa4caa0506994ddc833aae2e02087ab58b0224
Yuvraj <yuvrajbiswalofficial@gmail.com>
2026-03-19 10:00:00 +0530

    trying to read image files

---
+++ cmd/godec/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"image"
+	_ "image/jpeg"
+	"fmt"
+	"log"
+)
+
+func main() {
+	file,err := os.Open("../../assets/power.jpeg")
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	img, imageType, err := image.Decode(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Image format: %s\n", imageType)
+	fmt.Printf("Image bounds: %v\n", img.Bounds())
+}
